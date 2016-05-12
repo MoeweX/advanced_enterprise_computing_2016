0fcd3bfd96010a04de64e30d383a54e97f8db718
JonathanH5 <Jonathan@Hasenburg.de>
2016-05-12 13:42:18 +0200

    Add information on what we need

--- src/main/java/aec/Mastermind.java
+++ src/main/java/aec/Mastermind.java
@@ -2,6 +2,17 @@ package aec;
 
 public class Mastermind {
 
+	/*
+	 * Wir brauchen:
+	 * 	XML Leser <- Anne
+	 *  Konfigurationsklasse, in der XML Informationen für eigenen Knoten gesichert werden
+	 *  	HashMap mit Ziel als Key und Enum-Value für Methode
+	 *  Kommunikationsklasse, die weiß wie die anderen Maschinenen erreichbar sind (Dropbox)
+	 *  	-> organisiert auch, wann die Speicherklasse etwas speichern darf, etc.
+	 *  Speicherklasse, die Informationen sichert und logt (log by log4j) <- Jonathan
+	 */
+	
+	
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 	}
